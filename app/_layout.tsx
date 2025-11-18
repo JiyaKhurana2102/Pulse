5e494d9e6dda0d931e9c62e296e8d2016d597fb3
Om Pansuriya <dal651445@utdallas.edu>
2025-11-18 02:25:51 -0600

    loadingscreen for fun

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
+import LoadingScreen from '@/components/LoadingScreen';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useEffect, useState } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +13,23 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <StatusBar style="auto" />
+        <LoadingScreen />
+      </>
+    );
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
